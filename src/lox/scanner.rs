ac056ad8b3ae0a402173de38193ed2ac54f1614f
ebiazo <ebiazo@researchinnovations.com>
2021-11-11 22:38:56 -0600

    adding scanner

--- src/lox/scanner.rs
+++ src/lox/scanner.rs
@@ -1,13 +1,192 @@
+use std::fmt;
+use std::any::Any;
+
+pub enum TokenType {
+    // Single-character tokens.
+    LeftParen,
+    RightParen,
+    LeftBrace,
+    RightBrace,
+    Comma,
+    Dot,
+    Minus,
+    Plus,
+    SemiColon,
+    Slash,
+    Star,
+
+    // One or two character tokens.
+    Bang,
+    BangEqual,
+    Equal,
+    EqualEqual,
+    Greater,
+    GreaterEqual,
+    Less,
+    LessEqual,
+
+    // Literals.
+    Identifier,
+    String,
+    Number,
+
+    // Keywords.
+    And,
+    Class,
+    Else,
+    False,
+    Fun,
+    For,
+    If,
+    Nil,
+    Or,
+    Print,
+    Return,
+    Super,
+    This,
+    True,
+    Var,
+    While,
+
+    Eof,
+}
+impl fmt::Debug for TokenType {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            TokenType::LeftParen => write!(f, "LeftParen"),
+            TokenType::RightParen => write!(f, "RightParen"),
+            TokenType::LeftBrace => write!(f, "LeftBrace"),
+            TokenType::RightBrace => write!(f, "RightBrace"),
+            TokenType::Comma => write!(f, "Comma"),
+            TokenType::Dot => write!(f, "Dot"),
+            TokenType::Minus => write!(f, "Minus"),
+            TokenType::Plus => write!(f, "Plus"),
+            TokenType::SemiColon => write!(f, "SemiColon"),
+            TokenType::Slash => write!(f, "Slash"),
+            TokenType::Star => write!(f, "Star"),
+
+            TokenType::Bang => write!(f, "Bang"),
+            TokenType::BangEqual => write!(f, "BangEqual"),
+            TokenType::Equal => write!(f, "Equal"),
+            TokenType::EqualEqual => write!(f, "EqualEqual"),
+            TokenType::Greater => write!(f, "Greater"),
+            TokenType::GreaterEqual => write!(f, "GreaterEqual"),
+            TokenType::Less => write!(f, "Less"),
+            TokenType::LessEqual => write!(f, "LessEqual"),
+
+            TokenType::Identifier => write!(f, "Identifier"),
+            TokenType::String => write!(f, "String"),
+            TokenType::Number => write!(f, "Number"),
+
+            TokenType::And => write!(f, "And"),
+            TokenType::Class => write!(f, "Class"),
+            TokenType::Else => write!(f, "Else"),
+            TokenType::False => write!(f, "False"),
+            TokenType::Fun => write!(f, "Fun"),
+            TokenType::For => write!(f, "For"),
+            TokenType::If => write!(f, "If"),
+            TokenType::Nil => write!(f, "Nil"),
+            TokenType::Or => write!(f, "Or"),
+            TokenType::Print => write!(f, "Print"),
+            TokenType::Return => write!(f, "Return"),
+            TokenType::Super => write!(f, "Super"),
+            TokenType::This => write!(f, "This"),
+            TokenType::True => write!(f, "True"),
+            TokenType::Var => write!(f, "Var"),
+            TokenType::While => write!(f, "While"),
+
+            TokenType::Eof => write!(f, "Eof"),
+        }
+    }
+}
+
+pub struct Token<T> {
+    tok_type: TokenType,
+    lexme: String,
+    literal: T,
+    line: usize,
+}
+
+impl<T> fmt::Display for Token<T> {
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
+        write!(fmt, "{:?} {} {}", self.tok_type, self.lexme, self.line)
+    }
+}
+impl<T> Token<T> {
+    pub fn new(tok_type: TokenType, lexme: String, literal: T, line: usize) -> Token<T> {
+        Token {
+            tok_type,
+            lexme,
+            literal,
+            line,
+        }
+    }
+}
+enum Literal {
+    Null(Option<()>),
+    Str(String),
+    Int(usize),
+}
 pub struct Scanner {
     pub source: String,
+    current: usize,
+    start: usize,
+    line: usize,
+    tokens: Vec<Literal>,
 }
 
 impl Scanner {
     pub fn new(source: String) -> Self {
-        Scanner { source }
+        Scanner {
+            source: source,
+            current: 0,
+            start: 0,
+            line: 1,
+            tokens: Vec::new(),
+        }
     }
 
-    pub fn scan_tokens(&self) {
-        println!("{:?}", self.source);
+    fn advance(&self) -> char {
+        self.current += 1;
+        return self.source.chars().nth(self.current).expect("Index does not exist for source")
+    }
+
+    fn add_token(&self, tok_type: TokenType) {
+        add_token(type
+    }
+
+    fn add_token_lit(&self, toke_type: TokenType, lit: Literal) {
+
+    }
+    fn scan_token(&self) {
+        let mut c = self.advance();
+        match c {
+            '(' => self.add_token(TokenType::LeftParen),
+            ')' => self.add_token(TokenType::RightParen),
+            '{' => self.add_token(TokenType::LeftBrace),
+            '}' => self.add_token(TokenType::RightBrace),
+            ',' => self.add_token(TokenType::Comma),
+            '.' => self.add_token(TokenType::Dot),
+            '-' => self.add_token(TokenType::Minus),
+            '+' => self.add_token(TokenType::Plus),
+            ';' => self.add_token(TokenType::SemiColon),
+            '*' => self.add_token(TokenType::Star),
+            _ => assert!(true, "scan_token received character that is not implemented"),
+
+        }
+    }
+
+    pub fn scan_tokens(&mut self) {
+        while !self.is_at_end() {
+            self.start = self.current;
+            self.scan_token();
+        }
+
+        self.tokens
+            .push(Token::new(TokenType::Eof, String::from(""),Box::new(0) ,self.line));
+    }
+
+    fn is_at_end(&self) -> bool {
+        self.current >= self.source.chars().count()
     }
 }
